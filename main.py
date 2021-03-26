45ace1587131f8aabdcacb8233649c55ea05c0e5
tobiasosswald <56630570+tobiasosswald@users.noreply.github.com>
2021-03-26 17:32:03 +0100

    Update main.py
    removed features specific to original code to make it clearer and more general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# started Aug 18 2020
-# Postprocess Images. Detect cable.
+# v3 started on Mar 24
+# General edge detection
 
 ################ IMPORTS ###############################################
 from modules import *
@@ -7,96 +7,56 @@ from functions import *
 
 ################ MAIN ##################################################
 
-## read settings files
-test_matrix = pd.read_csv(
-	os.path.join("Settings","testMatrix.csv"),sep="	")
-idx_cases = np.where(test_matrix.process == "x")
-test_matrix = test_matrix.iloc[idx_cases[0]]
-ncases = len(test_matrix)
+### Read Settings Files
+# the test matrix contains the names of images to be processed 
+# and information on how to process each one
+img_info = pd.read_csv(
+	os.path.join("Settings","imageinfo.csv"),sep=",")
+idx_cases = np.where(img_info.process == "x")
+img_info = img_info.iloc[idx_cases[0]] # remove images wo/ x
+ncases = len(img_info)
+# the folder where the results are to be saved
 save = pd.read_csv(
 	os.path.join("Settings","saveTo.csv"),sep="	").iloc[0].directory
-datafolder = pd.read_csv(
-	os.path.join("Settings","dataFolder.csv")).iloc[0].directory
 if not os.path.isdir(save):
 	os.mkdir(save)
+# new folder with timestamp
+savedirect = os.path.join(save,str(datt.now())[:-7])
+if not os.path.isdir(savedirect):
+	os.mkdir(savedirect)
+# the folder containing all the images
+datafolder = pd.read_csv(
+	os.path.join("Settings","dataFolder.csv")).iloc[0].directory
 
-## cycle marked cases
-pbarc = tqdm(total=ncases)
+### cycle cases of img_info
+pbarc = tqdm(total=ncases) # progress bar
 for c in range(ncases):
 	# files, files..
-	direct = os.path.join(datafolder,test_matrix.iloc[c].Name)
-	# create file to save edges
-	savedirect = os.path.join(save,test_matrix.iloc[c].Name)
-	if not os.path.isdir(savedirect):
-		os.mkdir(savedirect)
-	else:
-		warnings.warn("A folder in the save directory of %s existed previously, files may be overwritten."%test_matrix.iloc[c].Name)
-	# list with all the images in the directory
-	fnames = dir_ftype(direct,".tif")
-	
-	## read processing parameters
-	calibration = pd.read_csv(os.path.join(
-		"Settings",test_matrix.iloc[c].calibration+".csv"),sep="	")
-	roi = np.s_[int(calibration.tl[1]):int(calibration.br[1]),
-		int(calibration.tl[0]):int(calibration.br[0])]# crop position
-	# read the camera calibration polynomial constants
-	Kx = np.array(calibration.Kx)[:10]
-	Ky = np.array(calibration.Ky)[:10]
-	# for the bead x-positions
-	nbead = pd.Series.last_valid_index(calibration.beadl)+1
-	beadsx = np.squeeze(
-		(calibration.beadl[:nbead],calibration.beadr[:nbead]))
+	direct = os.path.join(datafolder,img_info.iloc[c].Name)
 	# read the output file of tune_ghosal to get the parameters for edge
 	# detection, edge thresholding and blurring
 	pars = read_external_data(
-		os.path.join("Settings",str(test_matrix.iloc[c].GPars)))[0]
+		os.path.join("Settings",str(img_info.iloc[c].GPars)))[0]
 	K_s, k_min, k_max, l_max, phi_min, outlier_sigma, blur = \
 		int(pars[0]),pars[1],pars[2],pars[3],pars[4],pars[5],pars[6]
 
 	## detection and saving	
-	# initialization of variables
-	end = len(fnames)
-	pbar = tqdm(total=end)
-	# cycle timesteps
-	for i in range(0,end):
-		# open original image
-		img_o = read_image(fnames[i])
-		h, w = img_o.shape
-		# crop image
-		img_c = crop_image(img_o,roi)
-		# blur image
-		img_f = blurd_image(img_c,order=1,strength=blur,speed='fast')
-		# cycle beads
-		for b in range(nbead):
-			# isolating bead, take coordinate system into account
-			img = img_f[:,int(beadsx[0,b]-calibration.tl[0]):int(beadsx[1,b]-calibration.tl[0])]
-			# detect edges: Ghosal
-			edg_full, org = ghosal_edge_v2(img,K_s,kmax=k_max,
-				kmin=k_min,lmax=l_max,phimin=phi_min,mirror=True)
-			# outlier removal
-			pts,sig = line_fit(edg_full[:,1],edg_full[:,0])
-			ptsy = np.mean(pts[:,1])
-			edg_b = edg_full[(edg_full[:,0]<(ptsy+outlier_sigma*sig))
-				&(edg_full[:,0]>(ptsy-outlier_sigma*sig)),:]
-			# put edges in an array common coord syst (centered)
-			edg_b = edg_b+[calibration.tl[1]-h/2,beadsx[0,b]+calibration.tl[0]-w/2]
-			# convert to mm
-			edg_b[:,0] = poly_2d(edg_b[:,1],edg_b[:,0],Ky)
-			edg_b[:,1] = poly_2d(edg_b[:,1],edg_b[:,0],Kx)
-			if b==0:
-				edg = edg_b
-			else:
-				edg = np.concatenate((edg,edg_b))
-		fn = os.path.split(fnames[i])[1]
-		savename = os.path.join(savedirect,fn[:-4]+".txt")
-		np.savetxt(savename,edg,delimiter="	")
-		if i%100==0:
-			pbar.write("%s:	%d of	%d	found %d points"%(test_matrix.iloc[c].Name,i,end,len(edg[:,0])))
-		pbar.update(1)
-	pbar.close()
+	# open original image
+	img_o = read_image(direct)
+	h, w = img_o.shape
+	# blur (i.e filter) image
+	img_f = blurd_image(img_o,order=1,strength=blur,speed='fast')
+	# detect edges: Ghosal
+	edg_full, org = ghosal_edge_v2(img_f,K_s,kmax=k_max,
+		kmin=k_min,lmax=l_max,phimin=phi_min,mirror=True)
+	# save to savedirect with same name as img
+	savename = os.path.splitext(os.path.basename(direct))[0]+".txt"
+	savename = os.path.join(savedirect,savename)
+	np.savetxt(savename,edg_full,delimiter="	")
+	# progress bar
 	pbarc.update(1)
 pbarc.close()
 
-# for windows execution
+# for convenience
 input('Press ENTER to exit')
 	
